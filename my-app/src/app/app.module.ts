80cdd9f10d63e92fd2560128ee5961d782a6182f
Nicolay <you@example.com>
2020-03-24 21:01:25 +0700

    мы добавили компонент header

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
 
 @NgModule({
    declarations: [
       AppComponent,
+      MyHeaderComponent
    ],
    imports: [
       BrowserModule
