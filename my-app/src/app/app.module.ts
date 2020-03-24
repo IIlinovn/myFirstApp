3c42a4c2217b90f1a15f6847b7a540a7db819f2e
Nicolay <you@example.com>
2020-03-24 20:39:50 +0700

    ++

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from '.c:/Users/1/Desktop/1 angular/my-app/src/my-component/my-component.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      MyComponentComponent
    ],
    imports: [
       BrowserModule
